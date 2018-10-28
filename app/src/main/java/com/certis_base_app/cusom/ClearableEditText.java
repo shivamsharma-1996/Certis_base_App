b908d19bc6b6543c1058a0ffc386510da39f4772
shivamsharma-1996 <shivamsharma707@ahoocom>
2018-10-29 01:30:02 +0530

    registration step 1 starts

---
+++ app/src/main/java/com/certis_base_app/cusom/ClearableEditText.java
@@ -0,0 +1,124 @@
+package com.certis_base_app.cusom;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.certis_base_app.R;
+
+public class ClearableEditText extends AppCompatEditText implements View.OnTouchListener, View.OnFocusChangeListener, TextWatcher{
+
+    private Drawable mClearTextIcon;
+    private OnFocusChangeListener mOnFocusChangeListener;
+    private OnTouchListener mOnTouchListener;
+
+    public ClearableEditText(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (isFocused()) {
+            setClearIconVisible(s.length() > 0);
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    @Override
+    public void onFocusChange(final View view, final boolean hasFocus) {
+        if (hasFocus) {
+            setClearIconVisible(getText().length() > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+       /* if (mOnFocusChangeListener != null) {
+            mOnFocusChangeListener.onFocusChange(view, hasFocus);
+        }*/
+    }
+
+    @Override
+    public boolean onTouch(final View view, final MotionEvent motionEvent) {
+        final int x = (int) motionEvent.getX();
+        if (mClearTextIcon.isVisible() && x > getWidth() - getPaddingRight() - mClearTextIcon.getIntrinsicWidth()) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                setText("");
+            }
+            return true;
+        }
+        return mOnTouchListener != null && mOnTouchListener.onTouch(view, motionEvent);
+    }
+
+
+    @Override
+    public void setOnFocusChangeListener(final OnFocusChangeListener onFocusChangeListener) {
+        mOnFocusChangeListener = onFocusChangeListener;
+    }
+
+
+    @Override
+    public void setOnTouchListener(final OnTouchListener onTouchListener) {
+        mOnTouchListener = onTouchListener;
+    }
+
+
+    private void init(final Context context) {
+        final Drawable drawable = ContextCompat.getDrawable(context, R.drawable.ic_close_black_24dp);
+        final Drawable wrappedDrawable = DrawableCompat.wrap(drawable); //Wrap the drawable so that it can be tinted pre Lollipop
+        DrawableCompat.setTint(wrappedDrawable, getCurrentHintTextColor());
+        mClearTextIcon = wrappedDrawable;
+        mClearTextIcon.setBounds(0, 0, mClearTextIcon.getIntrinsicHeight(), mClearTextIcon.getIntrinsicHeight());
+        setCompoundDrawablePadding(5);
+        setClearIconVisible(false);
+        super.setOnTouchListener(this);
+        super.setOnFocusChangeListener(this);
+        addTextChangedListener(this);
+    }
+
+
+    private void setClearIconVisible(final boolean visible) {
+        mClearTextIcon.setVisible(visible, true);
+        final Drawable[] compoundDrawables = getCompoundDrawables();  // edittext has 4 compaoundDrawables to set drawable.
+        setCompoundDrawables(
+                compoundDrawables[0],
+                compoundDrawables[1],
+                visible ? mClearTextIcon : null,
+                compoundDrawables[3]);
+    }
+}
+
+
+
+
